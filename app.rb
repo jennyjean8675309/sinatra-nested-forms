04406e247a35a09674ad799fe98b1c78da0acd12
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2019-06-03 15:22:03 -0400

    Completed nested forms pirates lab

--- app.rb
+++ app.rb
@@ -2,8 +2,28 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    get '/' do
+      erb :root
+    end
 
-    # code other routes/actions here
+    get '/new' do
+      erb :'pirates/new'
+    end
 
+    post '/pirates' do
+      @pirate_name = params['pirate']['name']
+      @weight = params['pirate']['weight']
+      @height = params['pirate']['height']
+
+      @ship_1_name = params['pirate']['ships'][0]['name']
+      @ship_1_type = params['pirate']['ships'][0]['type']
+      @ship_1_booty = params['pirate']['ships'][0]['booty']
+
+      @ship_2_name = params['pirate']['ships'][1]['name']
+      @ship_2_type = params['pirate']['ships'][1]['type']
+      @ship_2_booty = params['pirate']['ships'][1]['booty']
+
+      erb :'pirates/show'
+    end
   end
 end
